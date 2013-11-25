70a5819921427558602aa142bf9a1b5f77b91ce7
Prashish <prashishh@gmail.com>
2013-11-25 00:35:58 -0500

    conwayGameRules done

--- script.js
+++ script.js
@@ -10,14 +10,20 @@ var ConwayGameOfLife = {
 	temp_game_space: [],
 
 	start: function() {
-		this.createGameBoard();
-		this.printGameBoard();
+		//this.createGameBoard();
+		//this.printGameBoard();
+		this.conwayGameRules(10);
 	},
 
 	createGameBoard : function() {
 		for ( var i = 0; i < 100; i++ ) {
 			this.game_space[i] = this.IsLiveOrDead();
 		}
+		this.copyGameBoard();
+	},
+
+	copyGameBoard : function() {
+		this.temp_game_space = this.game_space.concat();
 	},
 
 	IsLiveOrDead: function() {
@@ -26,18 +32,74 @@ var ConwayGameOfLife = {
 
 	printGameBoard: function() {
 		var temp_print_game = [];
-		var count = -1;		
-
-		for ( var i = 0; i < 100; i++ ) {
+		var count = 0;		
+		for ( var i = 0; i <= 100; i++ ) {
 			if( i % 10 == 0 ) {
-				if(count != -1) 
-					console.log(temp_print_game[count]);
+				console.log(temp_print_game[count]);
 				count++;
 				temp_print_game[count] = [];
 			}
 
 			temp_print_game[count].push(this.game_space[i]);
 		}
+		console.log(this.temp_game_space);
+	},
 
+	conwayGameRules: function(cell) {
+		var R = false, L = false, T = false, B = false;
+
+		// check top
+		if(cell <= 10) 
+			T = true;
+		// check bottom
+		if(cell > 89)
+			B = true;
+		// check right
+		if(cell % 10 == 0)
+			R = true;
+		// check left
+		if(String(cell)[1] == '1' || cell == 1)
+			L = true;
+
+		return this.checkconwayGameRules(cell, T, R, B, L)		
+	},
+
+	checkconwayGameRules: function(cell, T, R, B, L) {
+		var surround = {};
+		surround[cell - 11] = true;
+		surround[cell - 10] = true;
+		surround[cell - 9] = true;
+		surround[cell - 1] = true;
+		surround[cell + 1] = true;
+		surround[cell + 9] = true;
+		surround[cell + 10] = true;
+		surround[cell + 11] = true;
+
+		if( T ) {
+			delete surround[cell - 11];
+			delete surround[cell - 10];
+			delete surround[cell - 9];		
+		} 
+
+		if( B ) {
+			delete surround[cell + 9];
+			delete surround[cell + 10];
+			delete surround[cell + 11];
+		}
+
+		if( L ) {
+			delete surround[cell - 11];
+			delete surround[cell - 1];
+			delete surround[cell + 9];
+		}
+		
+		if( R ) {
+			delete surround[cell - 9];
+			delete surround[cell + 1];
+			delete surround[cell + 11];
+		}
+
+		console.log(surround);
+		return true;
 	}
 }
