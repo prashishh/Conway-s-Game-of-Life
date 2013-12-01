b64f5783d75ba7eed07b67c0220b0eed1b639c48
Prashish <prashishh@gmail.com>
2013-11-30 22:08:39 -0500

    intial GA commit

--- C++/cgol.cpp
+++ C++/cgol.cpp
@@ -5,23 +5,31 @@
 #include <string>
 #include <sstream>
 #include <time.h>
+#include <map>
 
 using namespace std;
 
+int global_fitness = 20000;
+int iterations = 3;
+int global_count = 0;
+vector<string> global_game_space;
+vector<bool> init_game_space;
 vector<bool> game_space;
 vector<string> hash_game_space;
 vector<bool> temp_game_space;
+vector <int, int> final_game_space;
 
 int IsLiveOrDead() {
     return bool(rand() % 2);
 }
 
 void createGameBoard() {
-    srand( (unsigned)time( NULL ) );
+    srand( (unsigned)time(NULL));
+    game_space.clear();
     for(int i = 0; i < 100; i++) {
         game_space.push_back(IsLiveOrDead());
     }
-    
+    init_game_space = game_space;
     string temp = "";
     
     for(vector<bool>::iterator i = game_space.begin(); i != game_space.end(); ++i) {
@@ -29,6 +37,7 @@ void createGameBoard() {
         temp += to_string(*i); // this will print all the contents of *features*
     }
     hash_game_space.push_back(temp);
+
 }
 
 void printGameBoard(vector<bool> temp_space) {
@@ -100,6 +109,30 @@ int checkSurround(int cell, int bool_surround[]) {
     return count;
 }
 
+void mutation(string gene) {
+    int position = rand() % 100;
+    if(gene[position] == '1')
+        gene[position] = '0';
+    else
+        gene[position] = '1';
+}
+
+void crossover(string gene1, string gene2) {
+    int position = rand() % 100;
+    string temp1 = "";
+    string temp2 = "";
+    
+    for(int i = 0; i < 100; i++){
+        if(i < position){
+            temp1 += gene1[i];
+            temp2 += gene2[i];
+        } else {
+            temp1 += gene2[i];
+            temp2 += gene1[i];
+        }
+    }
+}
+
 bool checkconwayGameRules(int cell, bool T, bool R, bool B, bool L) {
     int surround[8] = {1, 1, 1, 1, 1, 1, 1, 1};
     
@@ -160,6 +193,16 @@ bool conwayGameRules(int cell) {
     return checkconwayGameRules(cell, T, R, B, L);
 }
 
+string convertToString(vector<bool> space) {
+    string temp = "";
+    
+    for(vector<bool>::iterator i = space.begin(); i != space.end(); ++i) {
+        //cout << to_string(*i);
+        temp += to_string(*i); // this will print all the contents of *features*
+    }
+    
+    return temp;
+}
 
 string startGameOfLife() {
     temp_game_space.clear();
@@ -171,31 +214,27 @@ string startGameOfLife() {
     }
     
     game_space = temp_game_space;
-    //printGameBoard(game_space);
-    //std::string joined = boost::algorithm::join(game_space, "");
     
-    string temp = "";
-    
-    for(vector<bool>::iterator i = temp_game_space.begin(); i != temp_game_space.end(); ++i) {
-        //cout << to_string(*i);
-        temp += to_string(*i); // this will print all the contents of *features*
-    }
-     
-    
-    
-    //cout << temp;
-    return temp;
+    return convertToString(temp_game_space);
 
+}
+
+void checkGlobalFitness(int fitness) {
+    if(fitness < global_fitness) {
+        global_fitness = fitness;
+        cout << "\nNew Fitness = " << global_fitness;
+    }
+        
 }
 
 void startGame() {
     int count = 1;
     string temp_numb;
-    cout << "\nBeginning new game.." << endl;
-    
+    hash_game_space.clear();
+    srand( (unsigned)time(NULL));
+
     //temp_numb = startGameOfLife();
     //cout << temp_numb;
-    
     while(1) {
         temp_numb = startGameOfLife();
         //cout << temp_numb;
@@ -207,24 +246,49 @@ void startGame() {
         /*
         for(vector<string>::iterator i = hash_game_space.begin(); i != hash_game_space.end(); ++i) {
             cout << *i << "\n";
-           // temp += to_string(*i); // this will print all the contents of *features*
+           // temp += to_string(*i);
         }
         */
             
         } else {
-            cout << "\n Game Ended at " << count;
-            printGameBoard(game_space);
+            cout << "\nGame Ended at " << count;
+            checkGlobalFitness(count);
+           // global_game_space.push_back(convertToString(init_game_space));
+            final_game_space[convertToString(init_game_space)] = count;
+           // printGameBoard(game_space);
+            break;
+        }
+        
+        if (count == 20000) {
+            cout << "\nGame Timeout: Generation > 20000";
             break;
         }
     }
-    
-    
 }
 
+void startGA() {
+    int count = 0;
+    while(count < iterations) {
+        cout << "\n\nBeginning new game..";
+        createGameBoard();
+        //printGameBoard(game_space);
+        startGame();
+        count++;
+    }
+}
+
+/*
+for(vector<string>::iterator i = global_game_space.begin(); i != global_game_space.end(); ++i) {
+    cout << *i << "\n";
+    // temp += to_string(*i); // this will print all the contents of *features*
+ }
+*/
 int main() {
-    createGameBoard();
-    printGameBoard(game_space);
-    startGame();
+    startGA();
+    
+    cout << global_fitness << "\n";
+   // cout << global_game_space.size() << "\n";
+    cout << final_game_space.size();
     return 0;
 }
 
